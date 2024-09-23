58f91ded845f6657a9d0b0c9810b3594c83f9095
Antonioexd <estradajrantonio12@gmail.com>
2024-09-23 13:58:25 -0500

    Seguimiento con Funciones y metodos. Nombre y suma

---
+++ Funciones/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_y_metodos
+{
+    internal class Program
+    {
+        static int sumar(int a, int b)
+        { 
+        return a + b;
+        }
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine($"hola, {nombre}!");
+            Console.ReadKey();
+        }
+
+        static void Main(string[] arg)
+        {
+            Saludar("Antonio");
+
+            int resultado = sumar (5, 7);
+            Console.WriteLine("El resultado de la suma es: " + resultado);
+            Console.ReadKey();  
+        }
+    }
+}
