e2a6fb086e4721cf7a67ff62663f7db2d5cac4bb
Antonioexd <estradajrantonio12@gmail.com>
2024-09-22 23:40:48 -0500

    Sube la carpeta Concep_Basicos con los últimos cambios

---
+++ Condiciones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condiciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Condiciones (if,else)
+            int edad = 0;  
+
+            Console.WriteLine("Introduza su edad: ");
+            edad = int.Parse(Console.ReadLine());   
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Eres mayor de edad");
+                Console.ReadKey();  
+            }
+            else
+            { 
+            Console.WriteLine("Eres menor de edad");
+            Console.ReadKey();
+            }
+        }
+    }
+}
